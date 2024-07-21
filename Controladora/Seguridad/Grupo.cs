86b1bc4c1487d505054b60a5d535316b6f748483
tomas arias <tomas.arias2001@gmail.com>
2024-07-21 11:06:09 -0300

    Arreglo del boton eliminar grupo

--- Controladora/Seguridad/Grupo.cs
+++ Controladora/Seguridad/Grupo.cs
@@ -52,45 +52,20 @@ namespace Controladora.Seguridad
         }
 
 
-        public void agregarGrupo(Modelo.Grupos grupo, List<Modelo.Permisos> nuevosPermisosGrupo)
+        public void agregarGrupo(Modelo.Grupos grupo)
         {
             Modelo.Contexto.Obtener_instancia().Grupos.Add(grupo);
-            if (nuevosPermisosGrupo != null && nuevosPermisosGrupo.Count > 0)
-            {
-                foreach (Modelo.Permisos permiso in nuevosPermisosGrupo)
-                {
-                    grupo.Permisos.Add(permiso);
-                }
-            }
             Modelo.Contexto.Obtener_instancia().SaveChanges();
         }
 
-        public void modificarGrupo(Modelo.Grupos grupo, List<Modelo.Permisos> nuevosPermisosGrupo, List<Modelo.Permisos> eliminarPermisoGrupo)
+        public void modificarGrupo(Modelo.Grupos grupo)
         {
             Modelo.Contexto.Obtener_instancia().Entry(grupo).State = System.Data.Entity.EntityState.Modified;
-
-            if (nuevosPermisosGrupo != null && nuevosPermisosGrupo.Count > 0)
-            {
-                foreach (Modelo.Permisos permiso in nuevosPermisosGrupo)
-                {
-                    grupo.Permisos.Add(permiso);
-                }
-            }
-
-            if (eliminarPermisoGrupo != null && eliminarPermisoGrupo.Count > 0)
-            {
-                foreach (Modelo.Permisos permiso in eliminarPermisoGrupo)
-                {
-                    grupo.Permisos.Remove(permiso);
-                }
-            }
-
             Modelo.Contexto.Obtener_instancia().SaveChanges();
         }
 
         public void eliminarGrupo(Modelo.Grupos grupo)
         {
-            grupo.estado = false;
             Modelo.Contexto.Obtener_instancia().Entry(grupo).State = System.Data.Entity.EntityState.Modified;
             Modelo.Contexto.Obtener_instancia().SaveChanges();
         }
